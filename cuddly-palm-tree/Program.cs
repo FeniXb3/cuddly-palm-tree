fa7e232c420a4ea7f92a6e4cbbfc59cfd965a99a
Konrad Gadzina <konrad@makegames.today>
2024-06-16 16:04:00 +0200

    Update Program.cs

--- cuddly-palm-tree/Program.cs
+++ cuddly-palm-tree/Program.cs
@@ -1,59 +1,37 @@
-﻿using System;
-					
+using System;
+
+/*
+
+#########
+#.......#
+#.......#
+#.......#
+#.......#
+#########
+
+*/
+
+
 public class Program
 {
 	public static void Main()
 	{
+		Console.CursorVisible = false;
+		Console.Clear();
 		Player hero = new Player();
+		Map map = new Map();
+		map.Display();
 		hero.Display();
 		while (true)
 		{
 			Point nextPosition = hero.GetNextPosition();
-			hero.MoveTo(nextPosition.x, nextPosition.y);
-			hero.Display();
+			if (map.IsPositionCorrect(nextPosition))
+			{
+				hero.MoveTo(nextPosition.x, nextPosition.y);
+				map.RedrawCell(hero.previousPosition);
+				hero.Display();
+			}
 		}
-	}
-}
-
-class Point
-{
-	public int x;
-	public int y;
-}
-
-class Player
-{
-	string visuals = "@";
-	Point position = new Point();
-	
-	public void Display()
-	{
-		Console.Write(visuals);
-		//@ at (2, 5)
-		Console.WriteLine($" at ({position.x}, {position.y})");
-	}
-	
-	public Point GetNextPosition()
-	{
-		Point point = new Point();
-		point.x = position.x;
-		point.y = position.y;
-		
-		ConsoleKeyInfo keyInfo = Console.ReadKey(true);
-		if (keyInfo.Key == ConsoleKey.D)
-		{
-			// dodaj 1 do x
-			point.x += 1;
-		}
-		
-		
-		return point;
-	}
-	
-	public void MoveTo(int targetX, int targetY)
-	{
-		position.x = targetX;
-		position.y = targetY;
 	}
 }
 
