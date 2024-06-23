339174c7061c9a7180db077bf86253756e325b19
Konrad Gadzina <konrad@makegames.today>
2024-06-23 15:21:45 +0200

    Move WindowToSmallToDrawException to separate file

--- cuddly-palm-tree/Map.cs
+++ cuddly-palm-tree/Map.cs
@@ -91,14 +91,3 @@ class Map
         return false;
     }
 }
-
-[Serializable]
-internal class WindowToSmallToDrawException : Exception
-{
-    public Point ExpectedSize { get; private set;}
-
-    public WindowToSmallToDrawException(Point expectedSize) : base($"Minimum required window size is ({expectedSize.X}, {expectedSize.Y})")
-    {
-        ExpectedSize = expectedSize;
-    }
-}
